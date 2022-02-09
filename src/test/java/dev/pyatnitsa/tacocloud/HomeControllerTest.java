bc8fb177487ef90e626727a8e36908bcd1376475
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-02-09 20:10:46 +0300

    Added config for view controller

--- src/test/java/dev/pyatnitsa/tacocloud/HomeControllerTest.java
+++ src/test/java/dev/pyatnitsa/tacocloud/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package dev.pyatnitsa.tacocloud;
 
-import dev.pyatnitsa.tacocloud.controller.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,7 +9,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.containsString;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
     @Autowired
