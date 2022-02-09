9bcf86a172782e1d0e9f3db7e33fb4332a69ee88
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-02-09 14:38:45 +0300

    Initial commit

---
+++ src/test/java/dev/pyatnitsa/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pyatnitsa.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
